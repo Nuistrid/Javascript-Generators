d4209e1fc2b86ec6ae4e7486df58d6d121944f7e
Nuistrid <Actinoid.wadas@gmail.com>
2016-07-28 21:13:09 -0500

    Update InitFunc.js
    Added the 3 first generators

--- InitFunc.js
+++ InitFunc.js
@@ -8,7 +8,6 @@ function RandP(min,max)
 {
 	//Random number between (inclusive) min and max, included and floats
 	return parseFloat(Math.random()*(max-min)+min);
-
 }
 
 function RandNshow(min,max,num)
@@ -42,4 +41,64 @@ function ChooseN(arr,num,w)
 		else if (w>0) K[i] = arr[Math.floor(Math.pow(Math.random(),w)*arr.length)];
 	}
 	return K.toString();
+}
+
+//Generates Bits
+function GenerateData()
+{
+	var Val=document.getElementById("Number of Bits").value;
+	var Div=document.getElementById("Slice Mark").value;
+	var Result=[];
+	var P=["0","1"];
+	for(i=0;i<Val;i++) {Result[i]=ChooseN(P,Div).replace(/,/g,"");}
+	document.getElementById('Data Field').innerHTML=Result.join(" ");
+}
+
+//Card Deck Shuffler
+function CardDeck()
+{
+	var Cd=["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"];
+	var H=[];
+	var S=[];
+	var D=[];
+	var C=[];
+	var Result=[];
+	var Dk=document.getElementById("Deck Count").value;
+	for(i=0;i<Cd.length;i++) {
+		S[i]=Cd[i]+" of Spades";
+		H[i]=Cd[i]+" of Hearts";
+		C[i]=Cd[i]+" of Clubs";	
+		D[i]=Cd[i]+" of Diamonds";}
+	if (Dk=="4") {P=S.concat(C,D,H);}
+	else if (Dk=="5") {
+		var St=[];
+		for(i=0;i<Cd.length;i++) {St[i]=Cd[i]+" of Stars";}
+		P=S.concat(C,D,H,St);}
+	else if (Dk=="6") {
+		var Rk=[];
+		var Wh=[];
+		for(i=0;i<Cd.length;i++) {
+			Rk[i]=Cd[i]+" of Rackets";
+			Wh[i]=Cd[i]+" of Wheels";}
+		P=S.concat(C,D,H,Rk,Wh);}
+	for(i=0;i<Dk*Cd.length;i++) {
+		var Q=Choose(P);
+		R=P.indexOf(Q);
+		Result[i]=(i+1)+": "+Q;
+		P=P.slice(0,R).concat(P.slice(R+1));}
+	document.getElementById('Text Field').innerHTML=Result.join("\n");
+}
+
+//Dice Roller
+function DiceRoll()
+{
+	var Num=document.getElementById("Dice Count").value;
+	var sides=[];
+	var number=[];
+	var result=[];
+	for(i=0;i<Num;i++) {
+		number[i]=prompt("How many of each? ("+(i+1)+")");
+		sides[i]=prompt("How many different sides? ("+(i+1)+")");
+		result[i]=("("+number[i]+"d"+sides[i]+")"+" -- ["+RandNshow(1,sides[i],number[i])+"]");}
+	document.getElementById('Text Field').innerHTML=result.join("\n");
 }
